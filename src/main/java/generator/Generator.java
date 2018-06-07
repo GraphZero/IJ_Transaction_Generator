e5cd5442d3353e2a4101c058e670592a2ec3fad4
Andrzej <@>
2018-06-07 14:50:34 +0200

    DOCKER ASSIGMENT + DI ASSIGMENT

--- src/main/java/generator/Generator.java
+++ src/main/java/generator/Generator.java
@@ -8,7 +8,6 @@ import generator.readers.items.CsvFileReader;
 import org.apache.commons.cli.ParseException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.IOException;
@@ -17,22 +16,31 @@ import java.io.IOException;
 public class Generator {
     private static final Logger logger = LogManager.getLogger(Generator.class);
 
+    public static void generateTransactions(CommandLineReader commandLineReader, CsvFileReader csvFileReader){
+        new Parser()
+                .parseCommandLine(commandLineReader.getCmd())
+                .ifPresent( generateTransactionCommand-> {
+                    try {
+                        FileTransactionGeneratorManager transactionGeneratorManager
+                                = FileTransactionGeneratorManager.setUpTransactionGenerator(csvFileReader, generateTransactionCommand);
+                        transactionGeneratorManager.generateTransactions();
+                    } catch (CsvFileReader.InputItemFileNotFoundException e) {
+                        logger.error("Couldn't find input file");
+                    }
+                });
+    }
+
     public static void main(String[] args) throws ParseException {
         CsvFileReader csvFileReader = new CsvFileReader();
         CommandLineReader commandLineReader = null;
+
+        commandLineReader = CommandLineReader.readCommandLines(args);
+
+        generateTransactions(commandLineReader, csvFileReader);
+
         try {
             commandLineReader = CommandLineReader.readCommandLines(ConfigurationReader.getCommands());
-            new Parser()
-                    .parseCommandLine(commandLineReader.getCmd())
-                    .ifPresent( generateTransactionCommand-> {
-                        try {
-                            FileTransactionGeneratorManager transactionGeneratorManager
-                                    = FileTransactionGeneratorManager.setUpTransactionGenerator(csvFileReader, generateTransactionCommand);
-                            transactionGeneratorManager.generateTransactions();
-                        } catch (CsvFileReader.InputItemFileNotFoundException e) {
-                            logger.error("Couldn't find input file");
-                        }
-                    });
+            generateTransactions(commandLineReader, csvFileReader);
         } catch (IOException e) {
             logger.error("Couldn't find settings file");
         }
