7cd9b2e399eb2c0cbf45dd08539e7b7730f5108b
Andrzej <@>
2018-06-07 14:04:59 +0200

    DEPENDENCY INJECTION ASSIGMENT

--- src/main/java/generator/Generator.java
+++ src/main/java/generator/Generator.java
@@ -29,7 +29,7 @@ public class Generator {
                            logger.error("Couldn't find input file");
                         }
             });
-        SpringApplication.run(Generator.class, args);
+        //SpringApplication.run(Generator.class, args);
     }
 
 }
